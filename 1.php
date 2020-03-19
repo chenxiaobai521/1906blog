242aa4c45a08cc4419327e79093c4e82ce70af28
cep <2150876090@qq.com>
2020-03-19 01:13:41 +0000

    选座

--- 1.php
+++ 1.php
@@ -1,2 +1,26 @@
 <?php
-echo 123;
+
+    for($i=1;$i<=100;$i++)
+    {
+        echo "&nbsp;&nbsp;<button class='aaa'>{$i}</button>&nbsp;&nbsp;";
+        if($i%10==0){
+            echo "<br>";
+        }
+    }
+
+?>
+<style>
+.color{
+    color:red;
+}
+</style>
+<script src="/jquery.js"></script>
+<script>
+    $(document).on("click",".aaa",function(){
+        if(window.confirm('你确定要选中此座位吗')){ 
+            $(this).addClass('color');
+        }else{ 
+            $(this).removeClass('color');
+        }
+    });
+</script>
